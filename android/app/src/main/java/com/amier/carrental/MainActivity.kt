034cb61bf1ebd487eb45639dd774a4183ba9e8e7
Amiruddin Irsyad <amiruddinid@gmail.com>
2024-11-22 16:57:40 +0700

    change app name

--- android/app/src/main/java/com/amier/carrental/MainActivity.kt
+++ android/app/src/main/java/com/amier/carrental/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.carrental
+package com.amier.carrental
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/amier/carrental/MainApplication.kt
+++ android/app/src/main/java/com/amier/carrental/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.carrental
+package com.amier.carrental
 
 import android.app.Application
 import com.facebook.react.PackageList
